aa72e87a4d6193495e8d9a91be22a352fa2a18e4
stijnvankasteren <stijn@vankasteren.net>
2026-01-17 17:52:29 +0100

    changes

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -47,3 +47,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 }
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard (scene as? UIWindowScene) != nil else { return }
+    }
+
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        guard let context = URLContexts.first else { return }
+        var options: [UIApplication.OpenURLOptionsKey: Any] = [:]
+        if let sourceApp = context.options.sourceApplication {
+            options[.sourceApplication] = sourceApp
+        }
+        if let annotation = context.options.annotation {
+            options[.annotation] = annotation
+        }
+        _ = ApplicationDelegateProxy.shared.application(UIApplication.shared, open: context.url, options: options)
+    }
+
+    func scene(_ scene: UIScene, continue userActivity: NSUserActivity) {
+        _ = ApplicationDelegateProxy.shared.application(UIApplication.shared, continue: userActivity, restorationHandler: { _ in })
+    }
+
+}
